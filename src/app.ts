7b178b4e738061cb675ba371ee659fd12f5b50a6
Tavilo Breno <breno_wk2@hotmail.com>
2024-04-27 02:15:18 -0300

    Criando as rotas da api e o teste de rota

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import config from 'config';
+import router from "./router";
+
+const port = config.get<number>('port');
 
 const app = express();
 
 app.use(express.json());
 
-const port = config.get<number>('port');
+app.use('/api/', router);
 
 app.listen(port, async() => {
     console.log(`A aplicação está funcionado na porta: ${port}`);
