d3c6a8e0c5713c00da5ff23d44be65b389c8ebef
Tavilo Breno <breno_wk2@hotmail.com>
2024-04-27 15:32:54 -0300

    Configurando o Morgan

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,14 @@ import config from 'config';
 import router from "./router";
 import db from '../config/db';
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 
 const port = config.get<number>('port');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(morganMiddleware);
 app.use('/api/', router);
 
 app.listen(port, async() => {
