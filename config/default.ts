30effd65dd299f5b9fc30142cf653ced86b95e33
Tavilo Breno <breno_wk2@hotmail.com>
2024-04-27 02:39:00 -0300

    Criando variáveis de ambiente

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,7 @@
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
 export default {
   port: 3000,
-  dbUri:
-    "mongodb+srv://tavilobreno09:WWHxy2IqT0g4MEVQ@cluster0.9hajvye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.9hajvye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
 };
